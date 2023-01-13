d92253cbb36be39578133c11f30875acbded4732
o7q <jev.o7q@gmail.com>
2023-01-12 18:16:46 -0600

    Minor release [v1.3.0.0]

--- src/img2rgb/img2rgb.py
+++ src/img2rgb/img2rgb.py
@@ -2,29 +2,27 @@ from PIL import Image
 import os
 import sys
 
-version = "v1.0.1"
+version = "v1.1.0"
 
-fileCount = next(os.walk("img2ascii\\convert_temp\\raw"))[2]
+dataRead = open("img2ascii\\_temp", "r")
+data_raw = dataRead.read()
+dataRead.close()
+
+data = data_raw.split("|")
+
+fileCount = next(os.walk(data[0] + "\\_cache"))[2]
 
 nameIndex = 1
 for list_item in fileCount:
-    framePath = "img2ascii\\convert_temp\\raw\\frame.raw." + str(nameIndex) + ".png"
+    framePath = data[0] + "\\_cache\\frame.raw." + str(nameIndex) + ".png"
 
-    widthRead = open("img2ascii\\convert_temp\\frame_width", "r")
-    frameWidth = widthRead.read()
-    widthRead.close()
-
-    heightRead = open("img2ascii\\convert_temp\\frame_height", "r")
-    frameHeight = heightRead.read()
-    heightRead.close()
-
-    frameWidth_int, frameHeight_int = int(frameWidth), int(frameHeight)
+    frameWidth_int, frameHeight_int = int(data[1]), int(data[2])
     area, x, y, rgb_str = frameWidth_int * frameHeight_int, 0, 0, ""
 
     frame = Image.open(framePath).convert('RGB')
     for i in range(area):
         rgb = frame.getpixel((x, y))
-        rgb_str += str(rgb[0]) + "," + str(rgb[1]) + "," + str(rgb[2]) + "|"
+        rgb_str += str(rgb[0]) + "!" + str(rgb[1]) + "!" + str(rgb[2]) + "|"
 
         x += 1
         if x == frameWidth_int:
@@ -32,7 +30,7 @@ for list_item in fileCount:
             x = 0
 
     print(" Converting [frame.raw." + str(nameIndex) + ".png] to RGB")
-    output_file = open("img2ascii\\convert_temp\\rgb\\frame.rgb." + str(nameIndex) + ".txt", "w")
+    output_file = open(data[0] + "\\_cache\\rgb\\frame.rgb." + str(nameIndex) + ".txt", "w")
     output_file.write(rgb_str[:-1])
     output_file.close()
 
